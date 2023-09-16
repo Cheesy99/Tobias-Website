b098c8c053fb955532c775fae8ff4b571e747fbd
Tobias Chisi <tobiaschisi@gmail.com>
2023-09-16 16:14:15 +0200

    I edited the read me file

---
+++ eventHandler.js
@@ -0,0 +1,20 @@
+
+const myStoryButton = document.getElementById('MyStory');
+
+myStoryButton.addEventListener('click', () => {
+    window.location.href = 'myStory.html';
+});
+
+
+const homeButton = document.getElementById('Home');
+
+homeButton.addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
+
+
+const myPassionsButton = document.getElementById('MyPassions');
+
+myPassionsButton.addEventListener('click', () => {
+    window.location.href = 'myPassionsButton';
+});
